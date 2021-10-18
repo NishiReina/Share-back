1bbaa790676ee0e5a388598109bbecb5f65f27c4
NishiReina <s1f101901195@iniad.org>
2021-10-18 15:17:58 +0900

    first

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\CommentController;
+
+Route::post('/v1/user', UserController::class);
+
+Route::apiResource('/v1/post', PostController::class)->only([
+    'index', 'store', 'show','destroy'
+]);
+
+Route::post('/v1/like', [LikeController::class, 'store']);
+Route::delete('/v1/like', [LikeController::class, 'destroy']);
+Route::get('/v1/like/count', [LikeController::class, 'countlikes']);
+Route::get('/v1/like', [LikeController::class, 'haslike']);
+
+Route::post('/v1/comment', [CommentController::class, 'store']);
+Route::get('/v1/comment', [CommentController::class, 'show']);
